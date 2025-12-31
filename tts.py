758e44746cc7a753568eb646ba345525c5ee1083
rfan sgari <erfanasgari21@gmail.com>
2025-12-31 18:31:01 +0330

    update readme

--- tts.py
+++ tts.py
@@ -4,19 +4,19 @@ import torch
 from safetensors.torch import load_file as load_safetensors
 
 
-class MedadTTS():
+class MedadTTS:
     def __init__(
-            self,
-            weights_path, 
-            device="cuda" if torch.cuda.is_available() else "cpu"
-        ):
-        
-        self.multilingual_model = mtl_tts.ChatterboxMultilingualTTS.from_pretrained(device=device)
+        self, weights_path, device="cuda" if torch.cuda.is_available() else "cpu"
+    ):
+
+        self.multilingual_model = mtl_tts.ChatterboxMultilingualTTS.from_pretrained(
+            device=device
+        )
         t3_state = load_safetensors(weights_path, device="cuda")
         self.multilingual_model.t3.load_state_dict(t3_state)
         self.multilingual_model.t3.to(device).eval()
-        print('model loaded')
-        
+        print("model loaded")
+
     def __call__(self, prompt, **options):
         # default options
         settings = {
@@ -27,9 +27,7 @@ class MedadTTS():
             "exaggeration": 0.3,
         }
         settings.update(options)
-        
-        wav = multilingual_model.generate(
-            prompt,
-            **options)
-        
+
+        wav = self.multilingual_model.generate(prompt, **options)
+
         return wav
